1370311c2fa81a70bb3b07f34710f5b5674a94d0
Felix Grünewald <felixgruenewald@outlook.de>
2023-09-14 17:02:41 +0200

    Added different behaviour for Belgium(Flanders/Wallonia)

--- R/country_codes.R
+++ R/country_codes.R
@@ -9,6 +9,16 @@
 #' @examples country_codes(df, "ches")
 country_codes <- function (dataset, source){
 
+  if("country_year_id" %in% colnames(dataset)){
+  # (for now only) CSES differs between Wallonia & Flanders, this needs to be reflected in the country var
+  # Creating helper var for Belgian region:
+  dataset <- dataset %>%
+    mutate(belgium = case_when(
+      startsWith(country_year_id, "BELF") ~ "Flanders",
+      startsWith(country_year_id, "BELW") ~ "Wallonia",
+      TRUE ~ NA
+    ))}
+
   if (as.character(source) %in% colnames(country_dict)){
 
     dataset[["country"]] <- NA
@@ -19,8 +29,18 @@ country_codes <- function (dataset, source){
           mutate(country = case_when(
             country_orig == country_dict[[x, source]] ~ country_dict[[x, "country"]],
             TRUE ~ country
-
           ))
+
+        if("belgium" %in% colnames(dataset)){
+        # Replace Belgium with its region where applicable
+        dataset <- dataset %>%
+          mutate(country = case_when(
+            country == "Belgium" & belgium == "Flanders" ~ "Belgium (Flanders)",
+            country == "Belgium" & belgium == "Wallonia" ~ "Belgium (Wallonia)",
+            country == "Belgium" & is.na(belgium) ~ "Belgium",
+            TRUE ~ country
+          ))}
+
     }
   } else {print("Countries not defined in `country_dict`")}
 
